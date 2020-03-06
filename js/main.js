c76bb0bac3e67ebe945c1dc53f5606f76134d5a7
josehoras <jose.horas@gmail.com>
2020-03-06 23:06:33 +0100

    Correct scroll freeze for mobile

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ var reverse = document.getElementById("nav-menu-left");
 
 var icon = normal !== null ? normal : reverse;
 var offsetY = window.pageYOffset;
+var maxwid;
 
 // Toggle the "menu-open" % "menu-opn-left" classes
 function toggle() {
@@ -13,19 +14,19 @@ function toggle() {
 	  var button = document.getElementById("menu");
 	  var site = document.getElementById("wrap");
 
-
 	  if (nav.className == "menu-open" || nav.className == "menu-open-left") {
 	  	  nav.className = "";
 	  	  button.className = "";
 	  	  button.style.right = '0px';
 
-				// site.style.color = "#222222";
-				site.style.top = '0px';
-				site.style.left = '0px'
-
 				site.style.position = 'relative';
+				site.style.top = '0px';
+				if (window.innerWidth > 742) {
+					site.style.left = '0px'
+				} else {
+					site.style.maxWidth = maxwid;
+				}
 				window.scrollTo({top: offsetY});
-
 	  } else if (reverse !== null) {
 	  	  nav.className += "menu-open-left";
 	  	  button.className += "btn-close";
@@ -35,16 +36,15 @@ function toggle() {
 	  	  button.className += "btn-close";
 				button.style.right = '12px';
 
-				// site.style.color = "blue";
-				if (window.innerWidth > 940) {
+				if (window.innerWidth > 742) {
 					wid = (window.innerWidth - 742) * 0.5;
 					site.style.left = wid + 'px';
 				} else {
+					maxwid = site.style.maxWidth;
 					site.style.maxWidth = window.innerWidth - 12 + 'px';
 				}
 				offsetY = window.pageYOffset;
 				site.style.top = -offsetY + 'px';
-
 				site.style.position = 'fixed';
 	    }
 	}
